1ece895377ead479df61ae875e516f375a0a8f28
henrydatei <31316025+henrydatei@users.noreply.github.com>
2023-06-16 14:32:57 +0200

    remove app seed comment

--- Website/apps/home/models.py
+++ Website/apps/home/models.py
@@ -1,8 +1,3 @@
-# -*- encoding: utf-8 -*-
-"""
-Copyright (c) 2019 - present AppSeed.us
-"""
-
 from django.db import models
 from django.contrib.auth.models import User
 from django.db.models.signals import post_save
